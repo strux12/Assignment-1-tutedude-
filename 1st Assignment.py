c019be60dcdc37d596612537a7fbe9f0270247a7
strux12 <savithreddyr@gmail.com>
2025-05-14 09:20:32 +0530

    Add files via upload
    Tutedude assignment 1

---
+++ 1st Assignment.py
@@ -0,0 +1,11 @@
+#Task 1: Perform Basic Mathematical Operations
+n1=int(input("Please enter a number of your choice :"))
+n2=int(input("Please enter another number :"))
+a=n1 + n2
+b=n1 - n2
+c=n1 * n2
+d=n1 / n2
+print("Result of addition ", (a))
+print("Result of subtraction ", (b))
+print("Result of multiplication" , (c))
+print("Result of division " , (d))
---
+++ Assignment 1 (Task 2).py
@@ -0,0 +1,9 @@
+#Task 2
+a=input("What is your first name ?")
+a1=input("What is your last name ?")
+print("Hello,",(a) ,(a1) , "Welcome to the python program" )
+
+
+
+
+
